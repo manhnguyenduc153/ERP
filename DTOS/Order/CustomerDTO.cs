234da3dbef234b2005091eeecb2920a29f93aefa
Tai Nguyen Van <buiblue123@gmail.com>
2025-10-24 11:01:17 +0700

    fix: change dto for ui

---
+++ DTOS/Order/CustomerDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ERP_API.DTOS.Order
+{
+    public class CustomerDTO
+    {
+        public int CustomerId { get; set; }
+        public string? CustomerName { get; set; }
+        public string? Contact { get; set; }
+
+    }
+}
---
+++ DTOS/Order/OrderDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ERP_API.DTOS.Order
+{
+    public class OrderDTO
+    {
+        public int OrderId { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public int? CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Contact { get; set; }
+        public List<OrderDetailDTO>? OrderDetails { get; set; }
+
+    }
+}
